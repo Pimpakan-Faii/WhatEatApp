11651009bed6a6a6ec6868f9dcbf38166e642a3f
Pimpakan-Faii <pimpakan_faii@gmail.com>
2022-01-13 23:00:34 +0700

    WhatEatApp

--- app/src/main/java/com/example/whateat/Menufood.java
+++ app/src/main/java/com/example/whateat/Menufood.java
@@ -1,10 +1,14 @@
 package com.example.whateat;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -12,49 +16,14 @@ import android.widget.TextView;
 
 public class Menufood extends AppCompatActivity {
 
-
-    int IMAGES = R.drawable.bt1;
-    String[] NAMES = {"เมนูประเภททอด", "เมนูประเภทต้ม", "เมนูข้าวผัด", "เมนูข้าวกะเพรา", "เมนูก๋วยเตี๋ยว", "หมมูย่าง", "ชาบู"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menufood);
         ListView listView = (ListView) findViewById(R.id.list);
 
-        FoodAdapter foodAdapter = new FoodAdapter();
-        listView.setAdapter(foodAdapter);
 
-    }
 
-    class FoodAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return NAMES.length;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.activity_listview, null);
-
-            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView3);
-            TextView textView = (TextView) convertView.findViewById(R.id.typefood);
-
-            imageView.setImageResource(IMAGES);
-            textView.setText(NAMES[i]);
-            return convertView;
-
-        }
     }
 }
 
--- app/src/main/java/com/example/whateat/Restaurants.java
+++ app/src/main/java/com/example/whateat/Restaurants.java
@@ -3,12 +3,57 @@ package com.example.whateat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Restaurants extends AppCompatActivity {
+
+    int IMAGES = R.drawable.bt1;
+    String[] NAMES = {"ร้านแอนบะหมี่หน้ามอ", "ร้านป๊า ม๊าอาหารตามสั่ง", "ร้านอาหารบ้านคุณลุง", "ร้านอาหารแม่ดอล่า", "ร้านครัวต้นดอกอ้อ", "วีทีแหนมเนือง"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        ListView listView = (ListView) findViewById(R.id.list1);
+
+        RestAdapter foodAdapter = new RestAdapter();
+        listView.setAdapter(foodAdapter);
+
+    }
+
+    class RestAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.activity_listview, null);
+
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.bg3);
+            TextView textView = (TextView) convertView.findViewById(R.id.resturaant);
+
+            imageView.setImageResource(IMAGES);
+            textView.setText(NAMES[i]);
+            return convertView;
+
+        }
     }
 }
+
