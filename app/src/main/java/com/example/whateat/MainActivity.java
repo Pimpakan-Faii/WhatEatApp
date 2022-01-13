08daf65ff02428b755f67b583ba25700da7551c5
Pimpakan-Faii <pimpakan_faii@gmail.com>
2022-01-13 17:44:17 +0700

    WhatEatApp

--- app/src/main/java/com/example/whateat/MainActivity.java
+++ app/src/main/java/com/example/whateat/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.whateat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton menu,a1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menu = (ImageButton) findViewById(R.id.menufood);
+        a1 = (ImageButton) findViewById(R.id.menufood);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Menufood.class);
+                startActivity(i);
+            }
+        });
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b = new Intent(MainActivity.this,Menufood.class);
+                startActivity(b);
+            }
+        });
     }
+
 }
