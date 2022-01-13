b85fe054010edd64cf795b9854b0c9fee91b076a
Pimpakan-Faii <pimpakan_faii@gmail.com>
2022-01-13 23:54:06 +0700

    WhatEatApp

--- app/src/main/java/com/example/whateat/Details.java
+++ app/src/main/java/com/example/whateat/Details.java
@@ -2,13 +2,36 @@ package com.example.whateat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Details extends AppCompatActivity {
 
+    Button viewdo,viewran;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        viewdo = (Button) findViewById(R.id.ran2);
+        viewran = (Button) findViewById(R.id.ran3);
+
+        viewdo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v1 = new Intent(Details.this,Restaurants.class);
+                startActivity(v1);
+            }
+        });
+        viewran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v2 = new Intent(Details.this,Random.class);
+                startActivity(v2);
+            }
+        });
     }
 }
