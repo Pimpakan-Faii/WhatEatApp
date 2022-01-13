907cdbf487af0d203e35df5f418be63b1c9fa987
Guideus <GGWPTH@gmail.com>
2022-01-13 23:22:15 +0700

    HelloS

--- app/src/main/java/com/example/whateat/Restaurants.java
+++ app/src/main/java/com/example/whateat/Restaurants.java
@@ -2,78 +2,31 @@ package com.example.whateat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class Restaurants extends AppCompatActivity {
 
-    Button b1,b2,b3,b4,b5;
-
-
-    /*int IMAGES = R.drawable.bt1;
-    String[] NAMES = {"ร้านแอนบะหมี่หน้ามอ", "ร้านป๊า ม๊าอาหารตามสั่ง", "ร้านอาหารบ้านคุณลุง", "ร้านอาหารแม่ดอล่า", "ร้านครัวต้นดอกอ้อ", "วีทีแหนมเนือง"};*/
+    int IMAGES = R.drawable.bt1;
+    String[] NAMES = {"ร้านแอนบะหมี่หน้ามอ", "ร้านป๊า ม๊าอาหารตามสั่ง", "ร้านอาหารบ้านคุณลุง", "ร้านอาหารแม่ดอล่า", "ร้านครัวต้นดอกอ้อ", "วีทีแหนมเนือง"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-       /* ListView listView = (ListView) findViewById(R.id.list1);
+        ListView listView = (ListView) findViewById(R.id.list1);
+
         RestAdapter foodAdapter = new RestAdapter();
-        listView.setAdapter(foodAdapter);*/
-        b1 = (Button) findViewById(R.id.button20);
-        b2 = (Button) findViewById(R.id.button);
-        b3 = (Button) findViewById(R.id.button2);
-        b4 = (Button) findViewById(R.id.button3);
-        b5 = (Button) findViewById(R.id.button11);
+        listView.setAdapter(foodAdapter);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent b12 = new Intent(Restaurants.this,Details.class);
-                startActivity(b12);
-            }
-        });
-
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent b13 = new Intent(Restaurants.this,Details.class);
-                startActivity(b13);
-            }
-        });
-
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent b14 = new Intent(Restaurants.this,Details.class);
-                startActivity(b14);
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent b15 = new Intent(Restaurants.this,Details.class);
-                startActivity(b15);
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent b16 = new Intent(Restaurants.this,Details.class);
-                startActivity(b16);
-            }
-        });
     }
 
-   /* class RestAdapter extends BaseAdapter {
+    class RestAdapter extends BaseAdapter {
         @Override
         public int getCount() {
             return NAMES.length;
@@ -101,6 +54,6 @@ public class Restaurants extends AppCompatActivity {
             return convertView;
 
         }
-    }*/
+    }
 }
 
